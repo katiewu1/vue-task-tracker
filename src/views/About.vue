fd0ee5a1ff45af04a0c2cb81aa60d188a8357f02
katiewu1 <83988618+katiewu1@users.noreply.github.com>
2022-01-01 22:19:56 +0100

    codealong and disable browser warning about non-props attributes

---
+++ src/views/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <h3>Version 1.0.0</h3>
+  <router-link to="/">Go Back</router-link>
+</template>
+
+<script>
+export default {
+  name: "About",
+  // props: {
+  //   showAddTask: Boolean,
+  // },
+  inheritAttrs: false, // disable 'Extraneous non-props attributes' warning
+};
+</script>
